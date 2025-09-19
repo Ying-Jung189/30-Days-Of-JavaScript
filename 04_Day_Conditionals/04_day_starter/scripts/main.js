defe1c19752b196fef931fd662bc477c9c4b01e0
Melody <melodychen@talk2yam.com>
2025-09-19 16:59:24 +0800

    day_4

--- 04_Day_Conditionals/04_day_starter/scripts/main.js
+++ 04_Day_Conditionals/04_day_starter/scripts/main.js
@@ -1,3 +1,39 @@
 // this is your main.js script
+//level1
+//1
+let a = 4;
+let b = 3;
+if(a > b){
+    console.log('a is greater than b');
+}
+a > b? console.log('a is greater than b'): console.log('a is less than b');
+//level2
+//1
+// let num = Number(prompt('Enter number'));
 
-alert('Open the browser console whenever you work on JavaScript')
+// if (num >= 80 && num <= 100) {
+//   console.log('A');
+// } else if (num > 70 && num <= 80) {
+//   console.log('B');
+// } else if (num > 60 && num <= 70) {
+//   console.log('C');
+// } else if (num > 50 && num <= 60) {
+//   console.log('D');
+// } else if (num >= 0 && num <= 50) {
+//   console.log('E');
+// } else {
+//   console.log('out of score');
+// }
+//switch不適合用數字範圍
+//level3
+let enterMonth = prompt('Enter a month').toUpperCase();
+
+switch (enterMonth) {
+  case 'JANUARY':
+  case 'FEBRUARY':
+  case 'DECEMBER':
+    console.log('winter');
+    break;
+  default:
+    console.log('not winter');
+}
